a6ca7e3139f26763b4b18355bb088d52b56fbe0a
b00gizm <b00gizm@gmail.com>
2013-06-21 22:04:36 +0200

    Decide between server and client

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,12 +1,25 @@
-# Public: Install Plex.app to /Applications.
+# Public: Install Plex server to /Applications.
 #
 # Examples
 #
-#   include plex
+#   include plex::server
 
-class plex {
-  package { 'Plex':
+class plex::server {
+  package { 'Plex-Server':
     provider => 'appdmg',
     source   => 'http://plex.r.worldssl.net/plex-media-server/0.9.7.28.33-f80a4a2/PlexMediaServer-0.9.7.28.33-f80a4a2-OSX.dmg'
+  }
+}
+
+# Public: Install Plex client to /Applications.
+#
+# Examples
+#
+#   include plex::client
+
+class plex::client {
+  package { 'Plex-Client':
+    provider => 'compressed_app',
+    source   => 'http://plex.r.worldssl.net/plex-laika/0.9.5.4/Plex-0.9.5.4-973998f.zip'
   }
 }
--- spec/classes/plex_spec.rb
+++ spec/classes/plex_spec.rb
@@ -1,10 +1,19 @@
 require 'spec_helper'
 
-describe 'plex' do
+describe 'plex::server' do
   it do
-    should contain_package('Plex').with({
+    should contain_package('Plex-Server').with({
       :provider => 'appdmg',
       :source   => 'http://plex.r.worldssl.net/plex-media-server/0.9.7.28.33-f80a4a2/PlexMediaServer-0.9.7.28.33-f80a4a2-OSX.dmg',
+    })
+  end
+end
+
+describe 'plex::client' do
+  it do
+    should contain_package('Plex-Client').with({
+      :provider => 'compressed_app',
+      :source   => 'http://plex.r.worldssl.net/plex-laika/0.9.5.4/Plex-0.9.5.4-973998f.zip',
     })
   end
 end
